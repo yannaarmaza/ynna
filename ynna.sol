98c7b9af0530f8dd685be475d8819c02f69da0aa
yannaarmaza <139463771+yannaarmaza@users.noreply.github.com>
2024-05-18 18:41:24 +0800

    Update ynna.sol

--- ynna.sol
+++ ynna.sol
@@ -4,24 +4,38 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleToken is ERC20 {
-    address private _owner;
+    address public owner;
+
+    event TokensMinted(address indexed to, uint256 amount);
+    event TokensBurned(address indexed from, uint256 amount);
 
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
-        _owner = msg.sender;
+        owner = msg.sender;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == _owner, "Only owner can call this function");
-        _;
+            require(msg.sender == owner, "Not the owner");
+            _;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+        emit TokensMinted(to, amount);
     }
 
-    function burn(uint256 amount) public {
+    function burn(uint256 amount) external {
         _burn(msg.sender, amount);
-    }  
-}
+        emit TokensBurned(msg.sender, amount);
+    }
 
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(recipient != address(0), "Invalid address");
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(recipient != address(0), "Invalid address");
+        return super.transferFrom(sender, recipient, amount);
+    }
+}
